6841e16417de23498c5c886bb5ebfc4e091dcfad
Scott Chacon <schacon@gmail.com>
2009-05-07 15:08:37 -0700

    more tests, and almost all of them pass

--- test-hggit.rb
+++ test-hggit.rb
@@ -4,6 +4,37 @@ require 'pp'
 
 URL = 'git+ssh://localhost/opt/test.git'
 
+#exit
+
+test_this('named branch') do
+  append_data('README', 'testing')
+  hg("commit -m 'test'") # 0
+  hg("mv README readme.txt")
+  hg("commit -m 'branch with readme'") # 1
+
+  hg("checkout 0")
+  hg("branch newfeature")
+  append_data('README', 'testing')
+  hg("commit -m 'branch2'") # 2
+  hg("update -C 1")
+  hg("merge 2") 
+  hg("commit -m 'merge'") # 3
+end
+
+test_this('rename in first branch') do
+  append_data('README', 'testing')
+  hg("commit -m 'test'") # 0
+  hg("mv README readme.txt")
+  hg("commit -m 'branch with readme'") # 1
+
+  hg("checkout 0")
+  append_data('README', 'testing')
+  hg("commit -m 'branch2'") # 2
+  hg("update -C 1")
+  hg("merge 2") 
+  hg("commit -m 'merge'") # 3
+end
+
 test_this('simple') do
   append_data('README', 'testing')
   hg("commit -m 'test'")
@@ -21,5 +52,28 @@ test_this('branches') do
   hg("update -C 1")
   hg("merge 2")
   hg("commit -m 'merge'")
+end
+
+test_this('rename') do
+  append_data('README', 'testing')
+  hg("commit -m 'test'")
+  hg("mv README readme.txt")
+  hg("commit -m 'rename'")
+end
+
+test_this('rename in second branch') do
+  append_data('README', 'testing')
+  hg("commit -m 'test'") # 0
+  append_data('README', 'testing')
+  hg("commit -m 'branch with readme'") # 1
+
+  hg("checkout 0")
+  append_data('README', 'testing')
+  hg("commit -m 'branch2'") # 2
+  hg("mv README readme.txt")
+  hg("commit -m 'branch2 rename'") # 3
+  hg("update -C 1")
+  hg("merge 3")
+  hg("commit -m 'merge'") # 4
 end
 
