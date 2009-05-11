b26ef5a25b73a0bdbbc63189970efed81fa2d0fc
Scott Chacon <schacon@gmail.com>
2009-05-11 09:54:59 -0700

    lots of tests running now

--- test-hggit.rb
+++ test-hggit.rb
@@ -3,6 +3,104 @@ require 'helpers'
 
 URL = 'git+ssh://localhost/opt/test.git'
 
+test_hg_hg('rename in both branches') do
+  append_data('README', 'testing')
+  hg("commit -m 'test'") # 0
+  hg("mv README readme.txt")
+  hg("commit -m 'branch with readme'") # 1
+  append_data('readme.txt', 'more work')
+  hg("commit -m 'more work'") # 2
+
+  hg("checkout 0")
+  append_data('README', 'testing')
+  hg("commit -m 'branch2'") # 3
+  hg("update -C 2")
+  hg("merge 3") 
+  hg("resolve -m readme.txt") 
+  hg("commit -m 'merge'") # 4
+  
+  append_data('README2', 'testing')
+  hg("commit -m 'test'") # 5
+  append_data('README2', 'testing')
+  hg("commit -m 'branch with readme'") # 6
+
+  hg("checkout 5")
+  append_data('README2', 'testing')
+  hg("commit -m 'branch2'") # 7
+  hg("mv README2 readme2.txt")
+  hg("commit -m 'branch2 rename'") # 8
+  hg("update -C 6")
+  hg("merge 8")
+  hg("commit -m 'merge'") # 9
+end
+
+exit
+
+test_hg_hg('rename early in first branch') do
+  append_data('README', 'testing')
+  hg("commit -m 'test'") # 0
+  hg("mv README readme.txt")
+  hg("commit -m 'branch with readme'") # 1
+  append_data('readme.txt', 'more work')
+  hg("commit -m 'more work'") # 2
+
+  hg("checkout 0")
+  append_data('README', 'testing')
+  hg("commit -m 'branch2'") # 3
+  hg("update -C 2")
+  hg("merge 3") 
+  hg("resolve -m readme.txt") 
+  hg("commit -m 'merge'") # 4
+end
+
+test_hg_hg('rename early in second branch') do
+  append_data('README', 'testing')
+  hg("commit -m 'test'") # 0
+  append_data('README', 'testing')
+  hg("commit -m 'branch with readme'") # 1
+
+  hg("checkout 0")
+  append_data('README', 'testing')
+  hg("commit -m 'branch2'") # 2
+  hg("mv README readme.txt")
+  hg("commit -m 'branch2 rename'") # 3
+  append_data('readme.txt', 'testing')
+  hg("commit -m 'more work'") # 4
+  hg("update -C 1")
+  hg("merge 4")
+  hg("commit -m 'merge'") # 5
+end
+
+test_hg_hg('rename in first branch') do
+  append_data('README', 'testing')
+  hg("commit -m 'test'") # 0
+  hg("mv README readme.txt")
+  hg("commit -m 'branch with readme'") # 1
+
+  hg("checkout 0")
+  append_data('README', 'testing')
+  hg("commit -m 'branch2'") # 2
+  hg("update -C 1")
+  hg("merge 2") 
+  hg("commit -m 'merge'") # 3
+end
+
+test_hg_hg('rename in second branch') do
+  append_data('README', 'testing')
+  hg("commit -m 'test'") # 0
+  append_data('README', 'testing')
+  hg("commit -m 'branch with readme'") # 1
+
+  hg("checkout 0")
+  append_data('README', 'testing')
+  hg("commit -m 'branch2'") # 2
+  hg("mv README readme.txt")
+  hg("commit -m 'branch2 rename'") # 3
+  hg("update -C 1")
+  hg("merge 3")
+  hg("commit -m 'merge'") # 4
+end
+
 test_git_hg_git('modes') do
   append_data('README', 'testing', 'git')
   `ln -s README r.txt`
@@ -11,8 +109,6 @@ test_git_hg_git('modes') do
   `chmod a+x run`
   git("commit -am 'test'")
 end
-
-#exit
 
 test_hg_hg('simple') do
   append_data('README', 'testing')
@@ -66,59 +162,6 @@ test_git_hg_git('simple git commit') do
   git("commit -m 'test'")
 end
 
-test_hg_hg('rename early in second branch') do
-  append_data('README', 'testing')
-  hg("commit -m 'test'") # 0
-  append_data('README', 'testing')
-  hg("commit -m 'branch with readme'") # 1
-
-  hg("checkout 0")
-  append_data('README', 'testing')
-  hg("commit -m 'branch2'") # 2
-  hg("mv README readme.txt")
-  hg("commit -m 'branch2 rename'") # 3
-  append_data('readme.txt', 'testing')
-  hg("commit -m 'more work'") # 4
-  hg("update -C 1")
-  hg("merge 4")
-  hg("commit -m 'merge'") # 5
-end
-
-
-test_hg_hg('rename early in first branch') do
-  append_data('README', 'testing')
-  hg("commit -m 'test'") # 0
-  hg("mv README readme.txt")
-  hg("commit -m 'branch with readme'") # 1
-  append_data('readme.txt', 'more work')
-  hg("commit -m 'more work'") # 2
-
-  hg("checkout 0")
-  append_data('README', 'testing')
-  hg("commit -m 'branch2'") # 3
-  hg("update -C 2")
-  hg("merge 3") 
-  hg("resolve -m readme.txt") 
-  hg("commit -m 'merge'") # 4
-end
-
-
-test_hg_hg('rename in second branch') do
-  append_data('README', 'testing')
-  hg("commit -m 'test'") # 0
-  append_data('README', 'testing')
-  hg("commit -m 'branch with readme'") # 1
-
-  hg("checkout 0")
-  append_data('README', 'testing')
-  hg("commit -m 'branch2'") # 2
-  hg("mv README readme.txt")
-  hg("commit -m 'branch2 rename'") # 3
-  hg("update -C 1")
-  hg("merge 3")
-  hg("commit -m 'merge'") # 4
-end
-
 test_hg_hg('named branch') do
   append_data('README', 'testing')
   hg("commit -m 'test'") # 0
@@ -127,20 +170,6 @@ test_hg_hg('named branch') do
 
   hg("checkout 0")
   hg("branch newfeature")
-  append_data('README', 'testing')
-  hg("commit -m 'branch2'") # 2
-  hg("update -C 1")
-  hg("merge 2") 
-  hg("commit -m 'merge'") # 3
-end
-
-test_hg_hg('rename in first branch') do
-  append_data('README', 'testing')
-  hg("commit -m 'test'") # 0
-  hg("mv README readme.txt")
-  hg("commit -m 'branch with readme'") # 1
-
-  hg("checkout 0")
   append_data('README', 'testing')
   hg("commit -m 'branch2'") # 2
   hg("update -C 1")
